c08a3e4eec6bdd32194ae8dd35d49262a68135cf
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-07-06 19:45:35 +0530

    Create translation.py

---
+++ bot/translation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @kunaldiwan
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hello {} - I'm Auto filter V2 Bot .
+
+I can help you to filter files automatically from channel to group .
+
+➡️ Just add me to your group and channel as admin .
+
+For more details hit /help ....
+
+Powered by @DevelopedBots 💖
+</b>"""    
+    
+    HELP_TEXT = """
+<b>Hi {}! I am Auto filter Bot V2 . I can help you to filter files .</b>
+
+<b><u>Helpful commands 👇</u></b>
+
+»» <code>/about</code> - Check about me ...
+
+»» <code>/add chat_id</code> - To connect a group with a channel .
+
+»» <code>/dell chat_id</code> - To disconnect a group with a channel.
+
+»» <code>/delall</code> - This command will disconnect all connected channel with the group and deletes all its file from DB .
+
+⚠️ Bot should be admin in both channel and group .
+
+<b>If you got any issues about Bot ask at @DevelopedBotz .</b>
+"""
+    
+    ABOUT_TEXT = """<b>➥ 🤖 Name</b> : <code> Auto Filter V2 Bot</code>
+    
+<b>➥ 👨‍💻 Creator</b> : <b><a href="https://t.me/kunaldiwan">Kunal diwan</a></b>
+
+<b>➥ 🏳‍🌈 Language</b> : <code>Python3</code>
+
+<b>➥ 📚 Library</b> : <a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a>
+
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+"""
