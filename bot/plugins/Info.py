f2c912713872f358168157b9ee23194d9b343d43
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-11 06:53:44 +0530

    ADDED INFO MODULE.... ⚙️

--- bot/plugins/Info.py
+++ bot/plugins/Info.py
@@ -1,4 +1,67 @@
-# KANGED FROM TROJANZHEX BY MR-JINN-OF-TG PLZ TO GIVE CREDITS FOR DEVS BEHIND IT. 
+# KANGED FROM TROJANZHEX BY @MR-JINN-OF-TG PLZ TO GIVE CREDITS FOR DEVS BEHIND IT. 
 
 from pyrogram import Client, filters
 from pyrogram.errors import UsernameInvalid, UsernameNotOccupied
+
+@Client.on_message(filters.command('info') & (filters.private | filters.group))
+async def showinfo(client, message):
+    try:
+        cmd, id = message.text.split(" ", 1)
+    except:
+        id = False
+        pass
+
+    if id:
+        if (len(id) == 10 or len(id) == 9):
+            try:
+                checkid = int(id)
+            except:
+                await message.reply_text("__Enter a valid USER ID__", quote=True, parse_mode="md")
+                return
+        else:
+            await message.reply_text("__Enter a valid USER ID__", quote=True, parse_mode="md")
+            return           
+
+        if Config.SAVE_USER == "yes":
+            name, username, dcid = await find_user(str(id))
+        else:
+            try:
+                user = await client.get_users(int(id))
+                name = str(user.first_name + (user.last_name or ""))
+                username = user.username
+                dcid = user.dc_id
+            except:
+                name = False
+                pass
+
+        if not name:
+            await message.reply_text("__USER Details not found!!__", quote=True, parse_mode="md")
+            return
+    else:
+        if message.reply_to_message:
+            name = str(message.reply_to_message.from_user.first_name\
+                    + (message.reply_to_message.from_user.last_name or ""))
+            id = message.reply_to_message.from_user.id
+            username = message.reply_to_message.from_user.username
+            dcid = message.reply_to_message.from_user.dc_id
+        else:
+            name = str(message.from_user.first_name\
+                    + (message.from_user.last_name or ""))
+            id = message.from_user.id
+            username = message.from_user.username
+            dcid = message.from_user.dc_id
+    
+    if not str(username) == "None":
+        user_name = f"@{username}"
+    else:
+        user_name = "none"
+
+    await message.reply_text(
+        f"<b>Name</b> : {name}\n\n"
+        f"<b>User ID</b> : <code>{id}</code>\n\n"
+        f"<b>Username</b> : {user_name}\n\n"
+        f"<b>Permanant USER link</b> : <a href='tg://user?id={id}'>Click here!</a>\n\n"
+        f"<b>DC ID</b> : {dcid}\n\n",
+        quote=True,
+        parse_mode="html"
+    )
