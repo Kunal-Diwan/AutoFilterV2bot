24d9da1201e9b022c617c8fee4fb560370c22142
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-11 06:51:32 +0530

    Create Info.py

---
+++ bot/plugins/Info.py
@@ -0,0 +1,4 @@
+# KANGED FROM TROJANZHEX BY MR-JINN-OF-TG PLZ TO GIVE CREDITS FOR DEVS BEHIND IT. 
+
+from pyrogram import Client, filters
+from pyrogram.errors import UsernameInvalid, UsernameNotOccupied
