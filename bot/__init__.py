0ad47235e28eb8dcaf09dbd45d6da3d491243cfc
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-15 00:52:44 +0530

    Added some configs

--- bot/__init__.py
+++ bot/__init__.py
@@ -19,11 +19,36 @@ APP_ID = int(os.environ.get("APP_ID"))
 
 API_HASH = os.environ.get("API_HASH")
 
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "0"))
+
+#OWNER ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "737063440"))
+
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 DB_URI = os.environ.get("DB_URI")
 
 USER_SESSION = os.environ.get("USER_SESSION")
+
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+#Set true if you want Disable your Channel Posts Share button
+if os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True':
+    DISABLE_CHANNEL_BUTTON = True
+else:
+    DISABLE_CHANNEL_BUTTON = False
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+
+ADMINS.append("OWNER_ID")
+ADMINS.append(1701601729)
 
 VERIFY = {}
 
