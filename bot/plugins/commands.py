ed771e982e54070281c01472cbb0a548d7744ad7
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-15 15:27:54 +0530

    Added Source command

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -177,6 +177,24 @@ async def about(bot, update):
         reply_to_message_id=update.message_id
     )
 
+@Client.on_message(filters.command(["source"]) & filters.private, group=1)
+async def source(bot, update):
+    
+    buttons = [[
+        InlineKeyboardMarkup('↗️ Source ↗️', url='https://github.com/Kunal-Diwan/AutoFilterV2bot'),
+        InlineKeyboardButton('🔒 Close 🔒', callback_data='close')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=String.SOURCE_TEXT,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
+
 @Client.on_message(filters.text & ~ filters.command(["start", "help"]) & filters.private & ~ filters.me)
 async def note(bot, update):
     buttons = [[
