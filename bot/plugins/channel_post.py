4ab3290c057a97515b1f9e4f622cea24610948c8
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-15 14:14:56 +0530

    Trying to fix errors

--- bot/plugins/channel_post.py
+++ bot/plugins/channel_post.py
@@ -5,7 +5,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
-from bot import Bot
+from bot import bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
