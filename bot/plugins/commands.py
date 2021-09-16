9cb0576abc4606d06560446a6d753655a1fff6fb
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-16 13:10:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -9,8 +9,8 @@ from bot import Translation # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
 from bot import FORCESUB_CHANNEL, ADMINS, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
-from database.support import users_info
-from database.sql import add_user, query_msg
+from bot.database.support import users_info
+from bot.database.sql import add_user, query_msg
 
 db = Database()
 
