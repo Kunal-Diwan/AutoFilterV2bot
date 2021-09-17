7397f6453ec8f9bbe9b25fb879979f134013580d
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-17 13:52:42 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -97,13 +97,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
+            text=Translation.START_TEXT,
             reply_markup = reply_markup,
             disable_web_page_preview = True,
             quote = True
