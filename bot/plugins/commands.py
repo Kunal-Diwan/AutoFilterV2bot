a5f8b8e9844c74ef064e56085b4ad36d8eef8dfe
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-28 11:37:26 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -230,3 +230,24 @@ async def note(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.regex(r'^🤖 About 🤖$') | filters.command('report'))
+async def _manage(_, msg):
+    how = '**Report a Problem** \n\n'
+    how += "If something **unexpected** happens, you can report it to us. (You can also suggest features.)\n\n"
+    how += '**Steps** \n'
+    how += '1) Try whatever you did again. If it shows the same unexpected thing, move to step 2 \n'
+    how += '2) Visit @StarkBotsChat and define your problem **completely**, i.e, what you expected and what happened instead.'
+    how += "If you don't get a reply, tag an admin."
+    await msg.reply(
+        how,
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton('Support Group', url='https://t.me/StarkBotsChat')]
+        ]),
+        quote=True
+    )
+
