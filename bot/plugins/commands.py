520b6fca792e1d75f89992561e9811a74ae052c7
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-28 12:16:06 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -214,7 +214,7 @@ async def source(bot, update):
 @Client.on_message(filters.regex(r'^🤖 About 🤖$'))
 async def _manage(_, msg):
     text=Translation.ABOUT_TEXT
-    text += "If you don't get a reply, tag an admin."
+    text += "<b>I am advance Auto Filter Bot . I can help you to auto filter files And videos from a channel in group directly by sending file name .</b>"
     await msg.reply(
         text,
         quote=True
