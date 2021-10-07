9834dfebf363cd4fe2b0be32e363f4818c856df4
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-10-07 20:15:31 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -122,7 +122,7 @@ async def start(bot, update):
         
         return
 
-    await bot.send_photo(
+    await bot.send_message(
         chat_id=update.chat.id,
         photo = 'https://telegra.ph/file/4e9baf69190f8a56482db.jpg',
         caption=Translation.START_TEXT.format(
@@ -220,41 +220,6 @@ async def _manage(_, msg):
         reply_markup=ReplyKeyboardMarkup(
 			[
 				['⬅️ Back','👨‍💻 Developer 👨‍💻']
-			],
-			one_time_keyboard=True,
-			resize_keyboard=True
-		)
-	)
-
-    await bot.send_photo(
-        chat_id=update.chat.id,
-        photo = 'https://telegra.ph/file/4e9baf69190f8a56482db.jpg',
-        caption=Translation.START_TEXT.format(
-                update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup(
-            [
-
-                [
-                    InlineKeyboardButton('➕ Add me to Chat ➕', url='http://t.me/AutoFilterV2bot?startgroup=true')
-                ],
-                [
-                    InlineKeyboardButton('📢 Updates 📢', url='https://t.me/DevelopedBots'),
-                    InlineKeyboardButton('💬 Support 💬', url='https://t.me/DevelopedBotz')
-                ],
-                [
-                    InlineKeyboardButton('🔒 Close 🔒', callback_data='close')
-                ]
-            ]
-        ), 
-        parse_mode="html", 
-        reply_to_message_id=update.message_id
-       )
-    await bot.send_message(
-		update.chat.id,
-		'Use below buttons to interact with me',
-		reply_markup=ReplyKeyboardMarkup(
-			[
-				['🤖 About 🤖','⚙️ Help ⚙️']
 			],
 			one_time_keyboard=True,
 			resize_keyboard=True
